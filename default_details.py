741249f0035acc607d36b4a86a64d1a4ed8311ec
kalol <chetanvijaydua@gmail.com>
2017-09-14 09:37:26 +0530

    commit 2

---
+++ default_details.py
@@ -0,0 +1,5 @@
+#default things
+spy_age	=	12;
+spy_name	=	"unknown";
+spy_rating	=	0;
+status_msg	=	["Hello,Whats Up","Hi,Whats Up"];
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+
 print ("\n\nLet's get started:\n");
 spy_name	=	raw_input("Plz enter ur secret name: ");
+spy_age		=	12;
 
 i=0;
 while(i==0):
@@ -13,10 +15,15 @@ while(i==0):
 spy_salutation	=	raw_input("\nPlz enter proper salutation(Mr or Mrs):");
 while(i==1):
 	if (spy_salutation == "Mr." or spy_salutation == "mr" or spy_salutation =="Mr" or spy_salutation == "Mrs." or spy_salutation == "mrs" or spy_salutation == "Mrs" or spy_salutation == "MR" or spy_salutation == "MR." or spy_salutation == "MRS" or spy_salutation == "MRS."):
-			i=0;
+		i=0;
 	else:
 		print ("Plz enter correct salutation...\n");
 		spy_salutation	=	raw_input();
-	
+		
+		
 spy_name	=	spy_salutation+ " " + spy_name;
 print("\n\nWelcome " +spy_name+ ".\nAur ki haal chaal.");
+
+print type(spy_age);
+
+
